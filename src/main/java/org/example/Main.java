21734263c6a4205564ec5fcde34bef32f16a7859
Juan Antonio Moreno <ja.morenofernandez@gmail.com>
2023-02-21 21:17:20 +0100

    firs commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Conexion conexion = new Conexion();
+
+        try( Connection cnx = conexion.get_connection() ) {
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
